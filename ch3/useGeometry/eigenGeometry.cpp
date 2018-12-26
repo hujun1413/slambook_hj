3362bbb165e4c0699e644234437ec59be3d3ac22
Jun Hu <hujun1413@gmail.com>
2018-12-26 17:49:22 +0800

    add comments to ch4/useSophus

--- ch3/useGeometry/eigenGeometry.cpp
+++ ch3/useGeometry/eigenGeometry.cpp
@@ -16,7 +16,7 @@ int main ( int argc, char** argv )
     // 3D 旋转矩阵直接使用 Matrix3d 或 Matrix3f
     Eigen::Matrix3d rotation_matrix = Eigen::Matrix3d::Identity();
     // 旋转向量使用 AngleAxis, 它底层不直接是Matrix，但运算可以当作矩阵（因为重载了运算符）
-    Eigen::AngleAxisd rotation_vector ( M_PI/4, Eigen::Vector3d ( 0,0,1 ) );     //沿 Z 轴旋转 45 度
+    Eigen::AngleAxisd rotation_vector ( M_PI/2, Eigen::Vector3d ( 0,0,1 ) );     //沿 Z 轴旋转 45 度
     cout .precision(3);
     cout<<"rotation matrix =\n"<<rotation_vector.matrix() <<endl;                //用matrix()转换成矩阵
     // 也可以直接赋值
--- ch4/useSophus/useSophus.cpp
+++ ch4/useSophus/useSophus.cpp
@@ -25,16 +25,19 @@ int main( int argc, char** argv )
     
     // 使用对数映射获得它的李代数
     Eigen::Vector3d so3 = SO3_R.log();
-    cout<<"so3 = "<<so3.transpose()<<endl;
+    cout<<"旋转向量a属于so3(R3，是旋转矩阵对应的李代数)，a = "<<so3.transpose()<<endl;
     // hat 为向量到反对称矩阵
-    cout<<"so3 hat=\n"<<Sophus::SO3::hat(so3)<<endl;
+    cout<<"so3 hat, 旋转向量a的反对称矩阵a^ = \n"<<Sophus::SO3::hat(so3)<<endl;
+    cout<<"SO3 matrix, 旋转向量a的指数映射为R = exp(a^) = "<<endl<<SO3_R.matrix()<<endl; //matrix函数返回的是旋转矩阵R
     // 相对的，vee为反对称到向量
     cout<<"so3 hat vee= "<<Sophus::SO3::vee( Sophus::SO3::hat(so3) ).transpose()<<endl; // transpose纯粹是为了输出美观一些
     
     // 增量扰动模型的更新
     Eigen::Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
     Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3)*SO3_R;
-    cout<<"SO3 updated = "<<SO3_updated<<endl;
+    cout<<"SO3 updated, 旋转向量a属于so3(R3)，a = "<<SO3_updated<<endl;
+    cout<<"SO3 updated, 旋转向量a的反对称矩阵a^ = \n"<<Sophus::SO3::hat(SO3_updated.log())<<endl;
+    cout<<"SO3 updated, 旋转向量a的指数映射为旋转矩阵R(R属于特殊正交群) = exp(a^) = "<<endl<<SO3_updated.matrix()<<endl; //matrix函数返回的是旋转矩阵R
     
     /********************萌萌的分割线*****************************/
     cout<<"************我是分割线*************"<<endl;
@@ -47,10 +50,11 @@ int main( int argc, char** argv )
     // 李代数se(3) 是一个六维向量，方便起见先typedef一下
     typedef Eigen::Matrix<double,6,1> Vector6d;
     Vector6d se3 = SE3_Rt.log();
-    cout<<"se3 = "<<se3.transpose()<<endl;
+    cout<<"b属于se3([t', 旋转向量a]属于R6，是变换矩阵对应的李代数), b= "<<se3.transpose()<<endl;
     // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
     // 同样的，有hat和vee两个算符
-    cout<<"se3 hat = "<<endl<<Sophus::SE3::hat(se3)<<endl;
+    cout<<"se3 hat, b的反对称矩阵b^ = \n"<<Sophus::SE3::hat(se3)<<endl;
+    cout<<"se3 matrix, b的指数映射为变换矩阵R(R属于特殊欧式群) = exp(b^) = "<<endl<<SE3_Rt.matrix()<<endl; //matrix函数返回的是旋转矩阵R
     cout<<"se3 hat vee = "<<Sophus::SE3::vee( Sophus::SE3::hat(se3) ).transpose()<<endl;
     
     // 最后，演示一下更新
@@ -58,7 +62,7 @@ int main( int argc, char** argv )
     update_se3.setZero();
     update_se3(0,0) = 1e-4d;
     Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt;
-    cout<<"SE3 updated = "<<endl<<SE3_updated.matrix()<<endl;
+    cout<<"SE3 updated, T = "<<endl<<SE3_updated.matrix()<<endl; //matrix函数返回的是变换矩阵T
     
     return 0;
 }
